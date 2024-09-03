e886bfca04edd5952300dceb0405bb02e2ab5f25
Diego Trigo <trigobol23@gmail.com>
2024-09-03 10:14:15 -0400

    index y presenter

--- src/presenter.js
+++ src/presenter.js
@@ -1,15 +1,39 @@
-import sumar from "./sumador";
+import { cambioPosicion } from "./auto.js";
 
-const first = document.querySelector("#primer-numero");
-const second = document.querySelector("#segundo-numero");
-const form = document.querySelector("#sumar-form");
-const div = document.querySelector("#resultado-div");
+const form = document.querySelector("#auto-form");
+const entradaComando = document.querySelector("#entrada-comando");
+const divPosInicial = document.querySelector("#pos-inicial");
+const divComandos = document.querySelector("#comandos");
+const divResultado = document.querySelector("#resultado-div");
 
 form.addEventListener("submit", (event) => {
-  event.preventDefault();
+    event.preventDefault();
 
-  const firstNumber = Number.parseInt(first.value);
-  const secondNumber = Number.parseInt(second.value);
+    const entrada = entradaComando.value.trim();
+    const { superficie, posicionInicial, direccionInicial, comandos } = dividirEntrada(entrada);
+    const [xMax, yMax] = superficie.split(',').map(Number);
 
-  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+    const resultado = cambioPosicion(posicionInicial, direccionInicial, comandos, xMax, yMax);
+
+    divPosInicial.innerHTML = `<p>Posición Inicial: ${posicionInicial} Dirección Inicial: ${direccionInicial}</p>`;
+    divComandos.innerHTML = `<p>Comandos: ${comandos}</p>`;
+    
+    if (resultado.error) {
+        divResultado.innerHTML = `<p>${resultado.error}</p>`;
+    } else {
+        divResultado.innerHTML = `<p>Posición Final: x: ${resultado.x}, y: ${resultado.y} Dirección: ${resultado.direccion}</p>`;
+    }
 });
+
+function dividirEntrada(entrada) {
+    const [superficie, posicionInicialComando, comandos] = entrada.split('/');
+    const pos = posicionInicialComando.slice(0, -1);
+    const dir = posicionInicialComando.slice(-1);
+    
+    return {
+        superficie,
+        posicionInicial: pos,
+        direccionInicial: dir,
+        comandos
+    };
+}
